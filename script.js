bc657902a07f7bf2226012566f348f952b1d3ca4
jhstudio.designer <jhawran@edu.cdv.pl>
2024-12-09 04:20:57 +0100

    1

--- script.js
+++ script.js
@@ -38,46 +38,45 @@ setTimeout(shoot, 0);
 setTimeout(shoot, 100);
 setTimeout(shoot, 200);
 
-
 //confetti on click
 card.addEventListener("click", () => {
   const count = 200,
-  defaults = {
-    origin: { y: 0.7 },
-  };
+    defaults = {
+      origin: { y: 1 },
+    };
 
-function fire(particleRatio, opts) {
-  confetti(
-    Object.assign({}, defaults, opts, {
-      particleCount: Math.floor(count * particleRatio),
-    })
-  );
-}
+  function fire(particleRatio, opts) {
+    confetti(
+      Object.assign({}, defaults, opts, {
+        particleCount: Math.floor(count * particleRatio),
+      })
+    );
+  }
 
-fire(0.25, {
-  spread: 26,
-  startVelocity: 55,
-});
+  fire(0.25, {
+    spread: 26,
+    startVelocity: 55,
+  });
 
-fire(0.2, {
-  spread: 60,
-});
+  fire(0.2, {
+    spread: 60,
+  });
 
-fire(0.35, {
-  spread: 100,
-  decay: 0.91,
-  scalar: 0.8,
-});
+  fire(0.35, {
+    spread: 100,
+    decay: 0.91,
+    scalar: 0.8,
+  });
 
-fire(0.1, {
-  spread: 120,
-  startVelocity: 25,
-  decay: 0.92,
-  scalar: 1.2,
-});
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 25,
+    decay: 0.92,
+    scalar: 1.2,
+  });
 
-fire(0.1, {
-  spread: 120,
-  startVelocity: 45,
-});
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 45,
+  });
 });
