b83722a0592efcd2558b8e8e09b713c133017351
jhstudio.designer <jhawran@edu.cdv.pl>
2024-12-09 03:03:05 +0100

    confettin on click added

--- script.js
+++ script.js
@@ -5,32 +5,80 @@ const card = document.getElementById("flip-card");
 card.addEventListener("click", () => {
   card.classList.toggle("flipped");
 });
-const defaults = {
-  spread: 360,
-  ticks: 50,
-  gravity: 0,
-  decay: 0.94,
-  startVelocity: 30,
-  shapes: ["star"],
-  colors: ["FFE400", "FFBD00", "E89400", "FFCA6C", "FDFFB8"],
-};
 
-function shoot() {
-  confetti({
-    ...defaults,
-    particleCount: 40,
-    scalar: 1.2,
-    shapes: ["star"],
-  });
+const duration = 2 * 1000,
+  animationEnd = Date.now() + duration,
+  defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
 
-  confetti({
-    ...defaults,
-    particleCount: 10,
-    scalar: 0.75,
-    shapes: ["circle"],
-  });
+function randomInRange(min, max) {
+  return Math.random() * (max - min) + min;
 }
 
-setTimeout(shoot, 0);
-setTimeout(shoot, 100);
-setTimeout(shoot, 200);
+const interval = setInterval(function () {
+  const timeLeft = animationEnd - Date.now();
+
+  if (timeLeft <= 0) {
+    return clearInterval(interval);
+  }
+
+  const particleCount = 50 * (timeLeft / duration);
+
+  // since particles fall down, start a bit higher than random
+
+  confetti(
+    Object.assign({}, defaults, {
+      particleCount,
+      origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+    })
+  );
+  confetti(
+    Object.assign({}, defaults, {
+      particleCount,
+      origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+    })
+  );
+}, 250);
+
+// confetti click event
+
+card.addEventListener("click", () => {
+  const count = 200,
+    defaults = {
+      origin: { y: 1 },
+    };
+
+  function fire(particleRatio, opts) {
+    confetti(
+      Object.assign({}, defaults, opts, {
+        particleCount: Math.floor(count * particleRatio),
+      })
+    );
+  }
+
+  fire(0.25, {
+    spread: 26,
+    startVelocity: 55,
+  });
+
+  fire(0.2, {
+    spread: 60,
+  });
+
+  fire(0.35, {
+    spread: 100,
+    decay: 0.91,
+    scalar: 0.8,
+  });
+
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 25,
+    decay: 0.92,
+    scalar: 1.2,
+  });
+
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 45,
+  });
+});
